dea441acba9cf2a2c2ad103170fec04e8ca14dae
PierminatorQ <pierrequintero@gmail.com>
2020-08-16 15:07:08 -0400

    tweet model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_182202) do
+ActiveRecord::Schema.define(version: 2020_08_16_190604) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_08_16_182202) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
